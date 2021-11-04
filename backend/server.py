75abf047da659504e15255861420dd0cd29200e0
ii-ko <mhdihsan90@gmail.com>
2021-11-05 05:35:42 +0700

    create schema

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,15 @@
 import datetime
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 class Articles(db.Model):
@@ -22,11 +23,32 @@ class Articles(db.Model):
         self.body = body
 
 
+# Create DB Schema
+class ArticlesSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'body', 'date')
+
+
+article_schema = ArticlesSchema()
+articles_schema = ArticlesSchema(many=True)
+
 
 # default route
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return jsonify({"Hello" : "World"})
+
+
+# route for post
+@app.route('/post', methods=['POST'])
+def post():
+    title = request.json['title']
+    body = request.json['body']
+
+    articles = Articles(title, body)
+    db.session.add(articles)
+    db.session.commit()
+    return articles_schema.jsonify(articles)
 
 
 if __name__ == '__main__':
