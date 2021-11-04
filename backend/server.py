98261310b6563a8bcbdb6df50021134c5252ff51
ii-ko <mhdihsan90@gmail.com>
2021-11-05 05:22:57 +0700

    membuat database

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,26 @@
+import datetime
+
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+
+
+class Articles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    body = db.Column(db.Text())
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
 
 
 # default route
