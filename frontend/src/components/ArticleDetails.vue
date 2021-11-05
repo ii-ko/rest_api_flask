d703c54dfd2bf4f2787dc7eff48b24569295f89f
ii-ko <mhdihsan90@gmail.com>
2021-11-05 20:49:49 +0700

    success delete data

--- frontend/src/components/ArticleDetails.vue
+++ frontend/src/components/ArticleDetails.vue
@@ -3,6 +3,8 @@
     <h2>{{ article.title }}</h2>
     <p>{{ article.body }}</p>
     <p>Published Date : {{ article.date }}</p>
+    <button class="btn btn-danger mx-3 mt-3" @click="deleteById">Delete</button>
+    <button class="btn btn-secondary mx-3 mt-3">Edit</button>
   </div>
 </template>
 
@@ -20,6 +22,23 @@ export default {
     },
   },
   methods: {
+    deleteById() {
+      fetch("http://localhost:8000/delete/" + this.id + "/", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then(() => {
+          // redirect to home
+          this.$router.push({
+            name: "home",
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
     getArticleData() {
       fetch("http://localhost:8000/get/" + this.id + "/", {
         method: "GET",
@@ -27,6 +46,7 @@ export default {
           "Content-Type": "application/json",
         },
       })
+        // redirect to home
         .then((resp) => resp.json())
         .then((data) => {
           this.article = data;
